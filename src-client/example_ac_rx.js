f4a2a897f5dbc0b1c10b785faed6f985838458b7
Nikolay Ignatyev <noilgt@gmail.com>
2016-10-19 23:15:03 +0300

    Refactoring

--- src-client/example_ac_rx.js
+++ src-client/example_ac_rx.js
@@ -4,20 +4,31 @@ import Rx from "rxjs/Rx";
 const $title = $("#title");
 const $results = $("#results");
 
-const keyUps$ = Rx.Observable.fromEvent($title, "keyup"); //Stream of events
-const queries$ = keyUps$
+
+Rx.Observable.fromEvent($title, "keyup")
   .map(e => e.target.value) // Stream of text
   .disctinctUntilChanged()  // Produce value if value has changed from previous
   .debounceTime(250)       // Delay with 500ms
-  .switchMap(query => getItems(query)); //Provides result from the 1st query that complete
+  .switchMap(getItems)
+  .subscribe(items => {
+    $results.empty();
+    $results.append(items.map(res => $("<li />").text(res)));
+  });
 
-queries$.subscribe(query => {
-  getItems(query)
-    .then(items => {
-      $results.empty();
-      $results.append(items.map(res => $("<li />").text(res)));
-    });
-});
+// const keyUps$ = Rx.Observable.fromEvent($title, "keyup"); //Stream of events
+// const queries$ = keyUps$
+//   .map(e => e.target.value) // Stream of text
+//   .disctinctUntilChanged()  // Produce value if value has changed from previous
+//   .debounceTime(250)       // Delay with 500ms
+//   .switchMap(query => getItems(query)); //Provides result from the 1st query that complete
+
+// queries$.subscribe(query => {
+//   getItems(query)
+//     .then(items => {
+//       $results.empty();
+//       $results.append(items.map(res => $("<li />").text(res)));
+//     });
+// });
 
 /**
  * Sample async request
